c738bd1643e296aebff9e5c15805d6238c3b3a29
Kenzo Defoy <kenzo.defoy@icloud.com>
2024-06-05 17:34:49 +0200

    index-front

--- app/models/profile_research.rb
+++ app/models/profile_research.rb
@@ -1,6 +1,6 @@
 class ProfileResearch < ApplicationRecord
   belongs_to :user
   belongs_to :flat
-  has_many :couples, dependent: :destroy
+  has_many :couples, class_name: 'Couple', foreign_key: 'first_profile_id'
   validates :location, presence: true
 end
