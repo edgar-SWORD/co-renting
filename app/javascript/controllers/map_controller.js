2a329a9d48b0f3ddcc6c3d7303e58df40ac4c8db
Kenzo Defoy <kenzo.defoy@icloud.com>
2024-06-07 12:13:57 +0200

    push fix

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-import mapboxgl from 'mapbox-gl' 
+import mapboxgl from 'mapbox-gl'
 import MapboxGeocoder from 'mapbox-gl-geocoder'
 
 // Connects to data-controller="map"
@@ -8,7 +8,6 @@ export default class extends Controller {
   static targets = ["address"]
   static values = {
     apiKey: String,
-    markers: Array
   }
 
   connect() {
@@ -30,13 +29,6 @@ export default class extends Controller {
     this.geocoder.on("result", (event) => this.#setInputValue(event))
     this.geocoder.on("clear", () => this.#clearInputValue())
 
-    this.map = new mapboxgl.Map({
-      container: this.element,
-      style: "mapbox://styles/mapbox/light-v11"
-    })
-
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
   }
 
   disconnect() {
@@ -50,20 +42,5 @@ export default class extends Controller {
 
   #clearInputValue() {
     this.addressTarget.value = ""
-  }
-
-  #addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      // const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(this.map);
-    });
-  }
-
-  #fitMapToMarkers() {
-    const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach((marker) => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
