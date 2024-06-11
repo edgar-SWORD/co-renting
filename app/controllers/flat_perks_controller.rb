c2ddce75e8f76b632bb329d67ab53b31440e57c6
edgar meier <edgar.meier@swordagency.com>
2024-06-11 17:51:35 +0200

    multi-form-9

--- app/controllers/flat_perks_controller.rb
+++ app/controllers/flat_perks_controller.rb
@@ -14,11 +14,12 @@ class FlatPerksController < ApplicationController
 
   def create
     @flat_perk = FlatPerk.new
-    if @flat_perk.save
-      redirect_to  new_flat_intake_flat_style_path
-    else
-      render :new, status: :unprocessable_entity
+    @flat = current_user.profile_researches.last.flat
+    params[:flat_perk][:perk].each do |perk_id|
+      FlatPerk.create(flat: @flat, perk_id: perk_id.to_i)
     end
 
+    redirect_to new_flat_intake_flat_style_path
   end
+
 end
