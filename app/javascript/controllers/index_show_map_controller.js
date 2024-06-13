e4a33e14ef571e4aa90890ddde87d7136850a3f8
Kenzo Defoy <kenzo.defoy@icloud.com>
2024-06-13 10:31:11 +0200

    Fixed some front things

--- app/javascript/controllers/index_show_map_controller.js
+++ app/javascript/controllers/index_show_map_controller.js
@@ -1,41 +1,62 @@
-import { Controller } from "@hotwired/stimulus"
-import mapboxgl from 'mapbox-gl'
-import MapboxGeocoder from 'mapbox-gl-geocoder'
+import { Controller } from "@hotwired/stimulus";
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from 'mapbox-gl-geocoder';
+
 // Connects to data-controller="index-show-map"
 export default class extends Controller {
-  static targets = ["address"]
+  static targets = ["address"];
   static values = {
     apiKey: String,
     markers: Array
-  }
-
+  };
 
   connect() {
     console.log(this.apiKeyValue);
-    mapboxgl.accessToken = this.apiKeyValue
-
+    mapboxgl.accessToken = this.apiKeyValue;
 
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/light-v11"
-    })
+    });
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
+    this.markers = [];
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
   }
 
   #addMarkersToMap() {
+    // Clear existing markers
+    this.markers.forEach(marker => marker.remove());
+    this.markers = [];
+
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      new mapboxgl.Marker()
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
+      const newMarker = new mapboxgl.Marker({ color: '#83C5BE' })
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
         .addTo(this.map);
+      this.markers.push(newMarker);
     });
   }
 
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
-    this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { maxZoom: 11, duration: 10})
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    this.map.fitBounds(bounds, { maxZoom: 11, duration: 10 });
+  }
+
+  updateMarkers(event) {
+    event.preventDefault();
+    const url = event.target.action;
+    const formData = new FormData(event.target);
+
+    fetch(`${url}.json?${new URLSearchParams(formData).toString()}`)
+      .then(response => response.json())
+      .then(data => {
+        this.markersValue = data.markers;
+        this.#addMarkersToMap();
+        this.#fitMapToMarkers();
+      });
   }
 }
+
